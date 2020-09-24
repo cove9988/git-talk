e6fed690aeaaf291c9a186ca9b65d2c24c6595ba
paulg <cove9988@gmail.com>
2020-09-24 10:49:07 +1000

    Update __init__.py

--- git_talk/lib/__init__.py
+++ git_talk/lib/__init__.py
@@ -1,5 +1,1 @@
-import git_talk.lib.gfunc as gfunc
-import git_talk.lib.cutie as cutie
-import git_talk.lib.config as config
-import git_talk.lib.github_api as github_api
-import git_talk.lib.changelog.main as changelog
+
